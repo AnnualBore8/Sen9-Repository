c27edb81713cc1c309f8cda65e0ab4a456aeff4f
AnnualBore8 <115866043+AnnualBore8@users.noreply.github.com>
2022-10-26 22:57:49 +0300

    Лабораторная работа Тема 2

---
+++ Лабораторная работа Тема 2/Task_1.py
@@ -0,0 +1,8 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+set_ = set(list_)
+sum_ = sum(set_)
+print(sum_)
+len_ = len(set_)
+print(len_)
+average = sum_ / len_
+print(round(average, 5))
